cf783be4ed8aedd3a775eaf196a298dd585a1850
Faudev <fauezo@gmail.com>
2023-10-26 09:04:46 -0300

    Initial commit

---
+++ Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace DadosDoFuncionarios
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+
+            return SalarioBruto -= Imposto;
+            
+        }
+        public void  AumentoDeSalario(double porc)  {
+
+            // double porc2 = (porc/100)*SalarioBruto ;
+            double salario = SalarioBruto + Imposto; 
+            double porc2 = (porc / 100) * salario+SalarioBruto;
+            Console.WriteLine("Dados Atualizados : "
+               + Nome
+               + ", R$ "
+               + porc2.ToString("f2", CultureInfo.InvariantCulture)
+               );
+        }
+
+    }
+    
+ }
+
+
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using DadosDoFuncionarios;
+using System.Globalization;
+
+namespace DadosdoFuncionarios
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario(); // Instancia o objeto 
+            Console.WriteLine("Informe os dados abaixo:");
+            Console.WriteLine();
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();//Apos Istanciar Objeto chama as varias que estão publicas e insere os dados
+            Console.Write("Salario Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            //Aqui vamos criar avarias para inserir com retornos dos dados
+
+            Console.WriteLine("Funcionário: "
+                + funcionario.Nome
+                + ", R$ "
+                + funcionario.SalarioLiquido().ToString("f2", CultureInfo.InvariantCulture)
+                );
+
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            funcionario.AumentoDeSalario(porcentagem);
+        }
+    }
+  
+}
